ba317fa7205ec6200ca8750c89a5624731622d24
sabrina071088 <73609836+sabrina071088@users.noreply.github.com>
2020-11-24 15:24:41 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,15 @@
+class box{
+    constructor(x,y,width,height){
+        var options={
+            'restitution':1
+        }
+        this.body=Bodies.rectangle(x,y,width,height)
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position
+        rectMode(CENTER)
+        fill(255)
+        rect(pos.x,pos.y,this.width,this.height)
+    }
+}
